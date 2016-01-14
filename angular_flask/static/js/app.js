40284c9e1d25a06ad7110a0187ec24623941ff48
Clarity-89 <homes89@ukr.net>
2016-01-14 20:32:21 +0200

    Working configuration

--- angular_flask/static/js/app.js
+++ angular_flask/static/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('AngularFlask', ['angularFlaskServices'])
+angular.module('AngularFlask', ['angularFlaskServices', 'ngRoute', 'ngMaterial'])
 	.config(['$routeProvider', '$locationProvider',
 		function($routeProvider, $locationProvider) {
 		$routeProvider
