26663a19dc506b695cb9873c84a99c3f6c19837f
Clarity-89 <homes89@ukr.net>
2016-01-15 20:17:05 +0200

    Set up palette

--- angular_flask/static/js/app.js
+++ angular_flask/static/js/app.js
@@ -1,35 +1,38 @@
 'use strict';
 
 angular.module('AngularFlask', ['angularFlaskServices', 'ngRoute', 'ngMaterial'])
-	.config(['$routeProvider', '$locationProvider',
-		function($routeProvider, $locationProvider) {
-		$routeProvider
-		.when('/', {
-			templateUrl: 'static/partials/landing.html',
-			controller: IndexController
-		})
-		.when('/about', {
-			templateUrl: 'static/partials/about.html',
-			controller: AboutController
-		})
-		.when('/post', {
-			templateUrl: 'static/partials/post-list.html',
-			controller: PostListController
-		})
-		.when('/post/:postId', {
-			templateUrl: '/static/partials/post-detail.html',
-			controller: PostDetailController
-		})
-		/* Create a "/blog" route that takes the user to the same place as "/post" */
-		.when('/blog', {
-			templateUrl: 'static/partials/about.html',
-			controller: PostListController
-		})
-		.otherwise({
-			redirectTo: '/'
-		})
-		;
+    .config(['$routeProvider', '$locationProvider', '$mdThemingProvider',
+        function ($routeProvider, $locationProvider, $mdThemingProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'static/partials/landing.html',
+                    controller: IndexController
+                })
+                .when('/about', {
+                    templateUrl: 'static/partials/about.html',
+                    controller: AboutController
+                })
+                .when('/post', {
+                    templateUrl: 'static/partials/post-list.html',
+                    controller: PostListController
+                })
+                .when('/post/:postId', {
+                    templateUrl: '/static/partials/post-detail.html',
+                    controller: PostDetailController
+                })
+                /* Create a "/blog" route that takes the user to the same place as "/post" */
+                .when('/blog', {
+                    templateUrl: 'static/partials/post-list.html',
+                    controller: PostListController
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue-grey')
+                .accentPalette('orange')
+                .backgroundPalette('grey');
 
-		$locationProvider.html5Mode(true);
-	}])
+            $locationProvider.html5Mode(true);
+        }])
 ;
