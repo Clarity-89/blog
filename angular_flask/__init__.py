a651e748164865a8ca658085819c1d978338824c
Clarity-89 <homes89@ukr.net>
2016-01-22 22:14:54 +0200

    Fix path for saving imgs

--- angular_flask/__init__.py
+++ angular_flask/__init__.py
@@ -5,12 +5,12 @@ from flask import render_template, send_from_directory, url_for
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname('data'))
-
+basedir_img = os.path.abspath(os.path.dirname('angular_flask'))
 app.config["SECRET_KEY"] = "\xed\x9c\xac\xcd4\x83k\xd1\x17\xd54\xe71\x03\xaf\xd8\x04\xe3\xcd\xaa\xf4\x97\x82\x1e"
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data/posts.db')
 app.config["DEBUG"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['UPLOAD_FOLDER'] = '/home/alex/dev/blog/angular_flask/static/img'
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir_img, 'angular_flask/static/img')
 
 import angular_flask.core
 import angular_flask.models
