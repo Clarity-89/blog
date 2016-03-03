e602f05f3e9f8165c55ec06752b53a02ea84c8fa
Clarity-89 <homes89@ukr.net>
2016-03-04 00:55:01 +0200

    Change db url

--- angular_flask/__init__.py
+++ angular_flask/__init__.py
@@ -4,7 +4,7 @@ import psycopg2
 import urlparse
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["ec2-54-228-246-19.eu-west-1.compute.amazonaws.com"])
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 conn = psycopg2.connect(
     database=url.path[1:],
@@ -20,6 +20,7 @@ basedir_img = os.path.abspath(os.path.dirname('angular_flask'))
 app.config["SECRET_KEY"] = "\xed\x9c\xac\xcd4\x83k\xd1\x17\xd54\xe71\x03\xaf\xd8\x04\xe3\xcd\xaa\xf4\x97\x82\x1e"
 #app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data/posts.db')
 #app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://admin:111111@localhost:5432/posts'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config["DEBUG"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['UPLOAD_FOLDER'] = os.path.join(basedir_img, 'angular_flask/static/img')
