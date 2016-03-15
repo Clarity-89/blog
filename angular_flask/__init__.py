48543d559d13bb9446f455d14ec3e8ae1ff4f2d7
Clarity-89 <homes89@ukr.net>
2016-03-15 13:28:22 +0200

    Set tru to config from pyfile

--- angular_flask/__init__.py
+++ angular_flask/__init__.py
@@ -3,12 +3,12 @@ from flask import Flask
 from flask_sslify import SSLify
 
 
-app = Flask(__name__, instance_path='/instance')
+app = Flask(__name__, instance_relative_config=True)
 if 'DYNO' in os.environ:
     sslify = SSLify(app)
 
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py', True)
 
 import angular_flask.core
 import angular_flask.models
