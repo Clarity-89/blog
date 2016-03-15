1511f6b81964f1ec2fda5e9776354ef9a5aeae85
Clarity-89 <homes89@ukr.net>
2016-03-15 13:59:20 +0200

    Update dev runserver

--- runserver.py
+++ runserver.py
@@ -6,5 +6,5 @@ def runserver():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
-"""if __name__ == '__main__':
-    runserver()"""
+if __name__ == '__main__':
+    runserver()
