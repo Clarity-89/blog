ec95fa44a801ac011b1c7547510ac083ae3f8fba
Clarity-89 <homes89@ukr.net>
2016-03-16 22:53:24 +0200

    Generate unique index for slug in case of name clashes

--- angular_flask/models.py
+++ angular_flask/models.py
@@ -1,11 +1,10 @@
-import translitcodec, re
+import translitcodec, re, itertools
 from datetime import datetime
 from angular_flask.core import db
 from angular_flask import app
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 from flask.ext.login import UserMixin
-from sqlalchemy.exc import IntegrityError
 
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
@@ -23,7 +22,6 @@ class Post(db.Model):
     slug = db.Column(db.String(240), unique=True)
     body = db.Column(db.String())
     photo = db.Column(db.String(), default='../img/covers/default.jpg')
-    # Store time as integer in milliseconds
     date = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     favorited = db.Column(db.Integer, default=0)
@@ -32,20 +30,23 @@ class Post(db.Model):
 
     def slugify(self, text, delim=u'-'):
         """
-        Generates an ASCII-only slug and saves it to db.
-        :param text: text to be slugified
+        Generates an ASCII-only slug with unique index in case of slug name clashes and saves it to db.
+        :param text: text to be made into slug
         :param delim: delimiter for slug
-        :return:
         """
         result = []
         for word in _punct_re.split(text.lower()):
             word = word.encode('translit/long')
             if word:
                 result.append(word)
-        slug = unicode(delim.join(result))
-        res = Post.query.filter_by(slug=slug).first()
-        if res:
-            slug = unicode(delim.join(result)) + '-1'
+
+        slug = orig = unicode(delim.join(result))
+
+        for x in itertools.count(0):
+            if not Post.query.filter_by(slug=slug).first():
+                break
+            slug = '%s-%d' % (orig, x)
+
         self.slug = slug
 
     @property
@@ -71,9 +72,11 @@ class Post(db.Model):
         self.photo = photo
         self.author = author
 
+    def get_slug(self):
+        return self.slug
 
-def __repr__(self):
-    return '<id {}>'.format(self.id)  # models for which we want to create API endpoints
+    def __repr__(self):
+        return self.id
 
 
 class User(db.Model, UserMixin):
