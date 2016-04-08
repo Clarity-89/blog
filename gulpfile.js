cc3709a5943347ebd3eda6bc001e768f3c020cef
Clarity-89 <homes89@ukr.net>
2016-04-08 15:47:21 +0300

    Add font-awesome to bundle

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ gulp.task('clean', function () {
     return del(['./angular_flask/static/dist']);
 });
 
+// Copies fonts to /dist (for Font Awesome)
+gulp.task('fonts', function () {
+    return gulp.src('./node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('./angular_flask/static/fonts'))
+});
 
 gulp.task('bundle-css', function () {
     return gulp.src('./gulp.config/bundle-css.config.js')
